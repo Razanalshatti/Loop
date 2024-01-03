66fe59aec9ab9931babe60c56e966c272aa8035a
Razan S A A Alshatti <83327@kfh.com>
2024-01-03 14:02:46 +0300

    Loop

--- Java_Loop/src/WeekDaysPrinter.java
+++ Java_Loop/src/WeekDaysPrinter.java
@@ -2,12 +2,10 @@ public class WeekDaysPrinter {
     public static void main(String[] args) throws Exception {
         String[] daysOfWeek = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
-        
-        for (String day : daysOfWeek) {
-            
-            if (!day.equals("Saturday") && !day.equals("Sunday")) {
-                System.out.println(day);
-            }
+        for (int i = 0; i < daysOfWeek.length; i++) {
+            if (i >= 5) break; // Stop the loop after printing Friday
+            System.out.println(daysOfWeek[i]);
+
         }
     }
 }
